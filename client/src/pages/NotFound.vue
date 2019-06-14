2adf53628b78955efe1ebc858eee22e3a8349953
Ilya Kalibrov <kalibrov.ilya@inlinepro.ru>
2019-06-14 12:54:59 +0300

    NotFound component
    redirect in case of bad url

---
+++ client/src/pages/NotFound.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="col-full">
+        <h1>Not found</h1>
+        <p>We couldn't find, what you've been looking for.
+            <router-link :to="{ name: 'Home' }">
+                Why don't you go home?
+            </router-link>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+h1 {
+    font-size: 100px;
+}
+
+p {
+    font-size: 50px;
+    font-weight: 100 
+}
+
+div {
+    text-align: center
+}
+</style>
+
+
