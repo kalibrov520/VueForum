6d91b5885488c08b7073255a7b8676b2e8fa90fb
Ilya Kalibrov <kalibrov.ilya@inlinepro.ru>
2019-06-26 16:18:05 +0300

    PostEditor without custom events

--- client/src/components/PostEditor.vue
+++ client/src/components/PostEditor.vue
@@ -1,50 +1,46 @@
 <template>
-  <form @submit.prevent="save">
-    <div class="form-group">
+    <form @submit.prevent="addPost">
+      <div class="form-group">
         <textarea
           name=""
           id=""
           cols="30"
           rows="10"
           class="form-input"
-          v-model="text"
+          v-model="newPostText"
         ></textarea>
-    </div>
-    <div class="form-actions">
-      <button class="btn-blue">Submit post</button>
-    </div>
-  </form>
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">Submit post</button>
+      </div>
+    </form>
 </template>
 
 <script>
 export default {
-    props: {
-        threadId: {
-            required: true
-        }
-    },
-
     data () {
         return {
-            text: ''
+            newPostText: ''
         }
     },
 
     methods: {
-        save () {
-            const postId = 'greatPost' + Math.random();
-            const post = {
-                text: this.text,
-                publishedAt: Math.floor(Date.now() / 1000),
-                threadId: this.threadId,
-                userId: 'jUjmgCurRRdzayqbRMO7aTG9X1G2',
-                '.key': postId
-            }
-
-            this.text = '' 
-
-            this.$emit('save', {post})
+      addPost () {
+        const postId = 'postNumber' + Math.random()
+        const post = {
+          text: this.newPostText,
+          publishedAt: Math.floor(Date.now() / 1000),
+          threadId: this.id,
+          userId: 'jUjmgCurRRdzayqbRMO7aTG9X1G2',
+          '.key': postId
         }
+        /*this.$set(sourceData.posts, postId, post)
+        this.$set(this.thread.posts, postId, postId)
+        this.$set(sourceData.users[post.userId].posts, postId, postId)
+        this.newPostText = ''*/
+
+        this.$emit('save-post', {post}) //так передается объект, если название перемнной и имени проперти совпадает, иначе {post: postName}
+      }
     }
 }
 </script>
--- client/src/pages/ThreadShow.vue
+++ client/src/pages/ThreadShow.vue
@@ -2,30 +2,19 @@
   <div class="col-large push-top">
     <h1>{{thread.title}}</h1>
     <PostList :posts="posts"/>
-    <form @submit.prevent="addPost">
-      <div class="form-group">
-        <textarea
-          name=""
-          id=""
-          cols="30"
-          rows="10"
-          class="form-input"
-          v-model="newPostText"
-        ></textarea>
-      </div>
-      <div class="form-actions">
-        <button class="btn-blue">Submit post</button>
-      </div>
-    </form>
+    <PostEditor />
   </div>
 </template>
 
 <script>
   import sourceData from '@/data'
   import PostList from '@/components/PostList'
+  import PostEditor from '@/components/PostEditor'
+
   export default {
     components: {
-      PostList
+      PostList,
+      PostEditor
     },
     props: {
       id: {
@@ -35,30 +24,14 @@
     },
     data () {
       return {
-        thread: sourceData.threads[this.id],
-        newPostText: ''
+        thread: sourceData.threads[this.id]
       }
     },
     computed: {
       posts () {
         const postIds = Object.values(this.thread.posts)
         return Object.values(sourceData.posts)
-      }
-    },
-    methods: {
-      addPost () {
-        const postId = 'postNumber' + Math.random()
-        const post = {
-          text: this.newPostText,
-          publishedAt: Math.floor(Date.now() / 1000),
-          threadId: this.id,
-          userId: 'jUjmgCurRRdzayqbRMO7aTG9X1G2',
-          '.key': postId
-        }
-        this.$set(sourceData.posts, postId, post)
-        this.$set(this.thread.posts, postId, postId)
-        this.$set(sourceData.users[post.userId].posts, postId, postId)
-        this.newPostText = ''
+          
       }
     }
   }
