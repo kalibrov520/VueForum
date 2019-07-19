a336521521de664323c59ae15b3d25fbb1256798
Ilya Kalibrov <kalibrov.ilya@inlinepro.ru>
2019-07-19 14:46:31 +0300

    redirect on publish

--- client/src/pages/ThreadCreate.vue
+++ client/src/pages/ThreadCreate.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="col-full push-top">
 
-     <h1>Create new thread in <i>{{forum.name}}</i></h1>
+    <h1>Create new thread in <i>{{forum.name}}</i></h1>
 
-     <form @submit.prevent="save">
+    <form @submit.prevent="save">
       <div class="form-group">
         <label for="thread_title">Title:</label>
         <input v-model="title" type="text" id="thread_title" class="form-input" name="title">
       </div>
 
-       <div class="form-group">
+      <div class="form-group">
         <label for="thread_content">Content:</label>
         <textarea v-model="text" id="thread_content" class="form-input" name="content" rows="8" cols="140"></textarea>
       </div>
 
-       <div class="btn-group">
-        <button class="btn btn-ghost">Cancel</button>
+      <div class="btn-group">
+        <button @click.prevent="cancel" class="btn btn-ghost">Cancel</button>
         <button class="btn btn-blue" type="submit" name="Publish">Publish </button>
       </div>
     </form>
@@ -23,29 +23,35 @@
 </template>
 
 <script>
-export default {
-    props: {
+    export default {
+      props: {
         forum: {
-            type: Object,
-            required: true
+          type: Object,
+          required: true
         }
-    },
-
-    data () {
+      },
+      data () {
         return {
-            title: '',
-            text: ''
+          title: '',
+          text: ''
         }
-    },
-
-    methods: {
+      },
+      methods: {
         save () {
-            this.$store.dispatch('createThread', {
-              forumId: this.forum['.key'],
-              title: this.title,
-              text: this.text
-            }) 
+          this.$store.dispatch('createThread', {
+            forumId: this.forum['.key'],
+            title: this.title,
+            text: this.text
+          }).then(thread => {
+            this.$router.push({name: 'ThreadShow', params: {id: thread['.key']}})
+          })
+        },
+        cancel () {
+          this.$router.push({name: 'Forum', params: {id: this.forum['.key']}})
         }
+      }
     }
-}
 </script>
+
+<style scoped>
+</style>
